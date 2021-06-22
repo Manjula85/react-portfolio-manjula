33d98d164255a57df89d2f5e85c5fac99d80f026
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-06-22 07:48:48 -0400

    setting up the project layout v1

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -1,13 +1,7 @@
 import React, { useState } from "react";
 import { Grid, Segment, Image, GridRow } from "semantic-ui-react";
 //importing all the image paths here...
-import dog_dash from "../../assets/portfolio/dog_dash.PNG";
-import coders_bay from "../../assets/portfolio/coders_bay.PNG";
-import covid_positivity from "../../assets/portfolio/covid_positivity.PNG";
-import nosql from "../../assets/portfolio/nosql.PNG";
-import note_taker from "../../assets/portfolio/note_taker.PNG";
-import orm_mapping from "../../assets/portfolio/orm_mapping.PNG";
-import timed_quiz from "../../assets/portfolio/timed_quiz.PNG";
+
 
 const ProjectList = () => {
   const [photos] = useState([
@@ -68,55 +62,41 @@ const ProjectList = () => {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras semper tempor luctus. Integer volutpat, tortor.",
     },
   ]);
-  //build\static\media\coders_bay.433e8425.PNG
+
   return (
-    // <div class="container">
-    //   <div class="row">
-    //     <div class="col">
-    //       <Image
-    //         src={coverImage}
-    //         style={{ width: "40%", height: "auto" }}
-    //         floated="left"
-    //         alt="cover"
-    //       //class="altProfile"
-    //       />
-    //     </div>
-    //     <div class="col">
-    //       Column
-    //     </div>
-    //     <div class="col">
-    //       Column
-    //     </div>
-    //   </div>
-    // </div >
-    
-  <div>
-    {photos.length ? (
-      <section className="ui three cards">
-        {photos.map((image) => (
-          <div className="ui fluid card">
-            <a href={image.link} target="_blank" rel="noreferrer">
-              <div className="image">
-                <img
-                  //src={`./assets/portfolio/${image.name}.PNG`
-                  src={require(`../../assets/portfolio/${image.name}.PNG`).default
-                    //require(`./assets/portfolio/nosql.PNG`).default
-                    //coverImage
-                  }
-                  alt={image.name}
-                  key={image.name}
-                />
-                <h4 style={{ color: '#a3a3c2' }}>{image.name}</h4>
-                <h4 style={{ color: '#a3a3c2' }}>{image.link}</h4>
+    <div>
+      <div class="grid-container">
+        {photos.length ? (
+          <section className="ui three cards">
+
+            {photos.map((image) => (
+              <div className="ui fluid card">
+
+                <div class="grid-item">
+                  <a href={image.link} target="_blank" rel="noreferrer">
+                    <div className="image">
+                      <img
+                        src={require(`../../assets/portfolio/${image.name}.PNG`).default
+                        }
+                        alt={image.name}
+                        key={image.name}
+                        className="img-thumbnail mx-1"
+                      />
+                      <h4 style={{ color: '#a3a3c2' }}>{image.name}</h4>
+                      <h4 style={{ color: '#a3a3c2' }}>{image.link}</h4>
+                    </div>
+                  </a>
+                </div>
+
               </div>
-            </a>
-          </div>
-        ))}
-      </section>
-    ) : (
-      <h3>No posts</h3>
-    )}
-  </div>
+            ))}
+
+          </section>
+        ) : (
+          <h3>No posts</h3>
+        )}
+      </div>
+    </div>
   );
 };
 export default ProjectList;
